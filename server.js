1b1d0e1da303cc85048c8635b319c8f86012379c
Logan Hollins <logan.hollins1@gmail.com>
2015-11-22 00:33:46 -0800

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port);
+}).listen(80);
 
 
 // var http = require('http');
